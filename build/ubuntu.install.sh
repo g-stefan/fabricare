5ac11e4cd81aab3d71a5df26a1271d8d436abbc4
Grigore Stefan <g_stefan@yahoo.com>
2022-12-15 22:32:09 +0200

    Update

--- build/ubuntu.install.sh
+++ build/ubuntu.install.sh
@@ -7,11 +7,14 @@
 echo "- $project: install"
 
 export platformPath=
+export platformPathStr=
 if [ "$platform" = "mingw32" ]; then
-	export platformPath=\$MINGW_MOUNT_POINT
+	export platformPath=$MINGW_MOUNT_POINT
+	export platformPathStr=\$MINGW_MOUNT_POINT
 fi
 if [ "$platform" = "mingw64" ]; then
-	export platformPath=\$MINGW_MOUNT_POINT
+	export platformPath=$MINGW_MOUNT_POINT
+	export platformPathStr=\$MINGW_MOUNT_POINT
 fi
 
 mkdir -p "$HOME/SDK$platformPath/bin"
@@ -21,11 +24,11 @@ mkdir -p "$HOME/SDK$platformPath/lib"
 if ! grep -q "# set SDK PATH if exists" "$HOME/.profile"; then
 	echo "" >> "$HOME/.profile"
 	echo "# set SDK PATH if exists" >> "$HOME/.profile"
-	echo "if [ -d \"\$HOME/SDK$platformPath/bin\" ] ; then" >> "$HOME/.profile"
-	echo "    PATH=\"\$HOME/SDK$platformPath/bin:\$PATH\"" >> "$HOME/.profile"
-	echo "    LD_LIBRARY_PATH=\"\$HOME/SDK$platformPath/bin:\$LD_LIBRARY_PATH\"" >> "$HOME/.profile"
+	echo "if [ -d \"\$HOME/SDK$platformPathStr/bin\" ] ; then" >> "$HOME/.profile"
+	echo "    PATH=\"\$HOME/SDK$platformPathStr/bin:\$PATH\"" >> "$HOME/.profile"
+	echo "    LD_LIBRARY_PATH=\"\$HOME/SDK$platformPathStr/bin:\$LD_LIBRARY_PATH\"" >> "$HOME/.profile"
 	echo "fi" >> "$HOME/.profile"
 	echo "" >> "$HOME/.profile"
 fi
 
-cp output/bin/fabricare "$HOME/SDK/bin/fabricare"
+cp output/bin/fabricare "$HOME/SDK$platformPath/bin/fabricare"
