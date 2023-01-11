7e3e5d37da614314617420f7424b49658a81cc6a
Grigore Stefan <g_stefan@yahoo.com>
2023-01-11 20:31:09 +0200

    Update

--- source/XYO/Fabricare/Internal/platform/mingw.js
+++ source/XYO/Fabricare/Internal/platform/mingw.js
@@ -7,7 +7,7 @@ global.pathHome = Shell.getenv("HOME");
 global.pathRepository = pathHome + "/SDK/" + Platform.name;
 global.pathRelease = pathRepository + "/release";
 
-Shell.setenv("PATH", pathRepository + "/bin:" + Shell.getenv("PATH"));
+Shell.setenv("PATH", pathRepository + "\\bin;" + Shell.getenv("PATH"));
 Shell.setenv("LD_LIBRARY_PATH", pathRepository + "/bin:" + Shell.getenv("LD_LIBRARY_PATH"));
 
 Fabricare.include("solution");
