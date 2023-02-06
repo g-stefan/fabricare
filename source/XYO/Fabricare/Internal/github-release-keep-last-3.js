8381d30b6d08334756207a3182464037fe2bdb4a
Grigore Stefan <g_stefan@yahoo.com>
2023-02-06 13:58:57 +0200

    Update

--- source/XYO/Fabricare/Internal/github-release-keep-last-3.js
+++ source/XYO/Fabricare/Internal/github-release-keep-last-3.js
@@ -20,7 +20,6 @@ if (!Script.isNil(Solution.githubRepository)) {
 
 var version = getVersion();
 
-Shell.system("github-release info --repo " + repository + " --tag \"v" + version + "\"");
 var json = JSON.decode(Fabricare.runInteractive("github-release info --repo " + repository + " --json"));
 if (Script.isNil(json)) {
 	return;
--- source/XYO/Fabricare/Internal/github-release.js
+++ source/XYO/Fabricare/Internal/github-release.js
@@ -26,27 +26,54 @@ var releaseName = repository + "-" + version + "-" + Platform.name;
 Console.writeLn("Release v" + version);
 
 Shell.system("git pull --tags origin main");
-if (!Shell.system("git rev-parse --quiet \"v" + version + "\"")) {
-	Console.writeLn("release v" + version + " already exists");
+if (Shell.system("git rev-parse --quiet \"v" + version + "\"")) {
+
+	// Create release
+	Shell.system("git tag -a \"v" + version + "\" -m \"v" + version + "\"");
+	Shell.system("git push --tags");
+	Console.writeLn("Create release v" + version);
+	Shell.system("github-release release --repo " + repository + " --tag \"v" + version + "\" --name \"v" + version + "\" --description \"Release\"");
+
+	// Wait a little for github to update release info
+	CurrentThread.sleep(1500);
+	Shell.system("github-release info --repo " + repository + " --tag \"v" + version + "\"");
+
+};
+
+var version = getVersion();
+
+var json = JSON.decode(Fabricare.runInteractive("github-release info --repo " + repository + " --tag \"v" + version + "\" --json"));
+if (Script.isNil(json)) {
+	Console.writeLn("Release not found for version " + version);
 	return;
 };
-Shell.system("git tag -a \"v" + version + "\" -m \"v" + version + "\"");
-Shell.system("git push --tags");
-Console.writeLn("Create release v" + version);
-Shell.system("github-release release --repo " + repository + " --tag \"v" + version + "\" --name \"v" + version + "\" --description \"Release\"");
 
-// Wait a little for github to update release info
-CurrentThread.sleep(1500);
-Shell.system("github-release info --repo " + repository + " --tag \"v" + version + "\"");
+var releaseToCheck = json["Releases"];
+var i, j;
+var releaseList = {};
+
+for (i = 0; i < releaseToCheck.length; ++i) {
+	Console.writeLn("Check release " + releaseToCheck[i]["tag_name"]);
+	for (j = 0; j < releaseToCheck[i].assets.length; ++j) {
+		Console.writeLn("\t- " + releaseToCheck[i].assets[j].name);
+		if (Shell.fileExists("release/" + releaseToCheck[i].assets[j].name)) {
+			releaseList["release/" + releaseToCheck[i].assets[j].name] = true;
+		};
+	};
+};
+
 
 var fileList = Shell.getFileList("release/*-" + version + "*.7z");
 for (var file of fileList) {
-	Console.writeLn("Upload " + Shell.getFileName(file));
-	Shell.system("github-release upload --repo " + repository + " --tag \"v" + version + "\" --name \"" + Shell.getFileName(file) + "\" --file \"" + file + "\"");
+	if(Script.isNil(releaseList[file])) {
+		Console.writeLn("Upload " + Shell.getFileName(file));
+		Shell.system("github-release upload --replace --repo " + repository + " --tag \"v" + version + "\" --name \"" + Shell.getFileName(file) + "\" --file \"" + file + "\"");
+	};
 };
 
 var fileList = Shell.getFileList("release/*-" + version + "*.json");
 for (var file of fileList) {
 	Console.writeLn("Upload " + Shell.getFileName(file));
-	Shell.system("github-release upload --repo " + repository + " --tag \"v" + version + "\" --name \"" + Shell.getFileName(file) + "\" --file \"" + file + "\"");
+	Shell.system("github-release upload --replace --repo " + repository + " --tag \"v" + version + "\" --name \"" + Shell.getFileName(file) + "\" --file \"" + file + "\"");
 };
+
