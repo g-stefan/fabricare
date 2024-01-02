7bf571bf41af381d17c9a3971103b0a99ff12cff
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:49:24 +0200

    Update

--- build/platform/wsl-ubuntu-20.04.cmd
+++ build/platform/wsl-ubuntu-20.04.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 rem Public domain (Unlicense) <http://unlicense.org>
-rem SPDX-FileCopyrightText: 2021-2023 Grigore Stefan <g_stefan@yahoo.com>
+rem SPDX-FileCopyrightText: 2021-2024 Grigore Stefan <g_stefan@yahoo.com>
 rem SPDX-License-Identifier: Unlicense
 
 ubuntu2004.exe -c "platform=ubuntu-20.04 ./build/platform/wsl.sh %1"
