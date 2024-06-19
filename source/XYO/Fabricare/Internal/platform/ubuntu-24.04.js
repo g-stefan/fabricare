2630672f9181fda7c7d2cc3b982b97f29ec69d12
Grigore Stefan <g_stefan@yahoo.com>
2024-06-20 01:19:53 +0300

    Update

---
+++ source/XYO/Fabricare/Internal/platform/ubuntu-24.04.js
@@ -0,0 +1,12 @@
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+// Public domain (Unlicense) <http://unlicense.org>
+// SPDX-FileCopyrightText: 2021-2024 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-License-Identifier: Unlicense
+
+Platform.name = "ubuntu-24.04";
+Platform.osName = "ubuntu";
+Platform.osType = "linux";
+Platform.version = "24.04";
+Platform.machine = "x64";
+
+Fabricare.include("platform/ubuntu.run");
---
+++ source/XYO/Fabricare/Internal/platform/wsl-ubuntu-24.04.js
@@ -0,0 +1,10 @@
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+// Public domain (Unlicense) <http://unlicense.org>
+// SPDX-FileCopyrightText: 2021-2024 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-License-Identifier: Unlicense
+
+Platform.name = "wsl-ubuntu-24.04";
+Platform.run = "ubuntu2404.exe";
+Platform.next = "ubuntu-24.04";
+
+Fabricare.include("platform/wsl.run");
